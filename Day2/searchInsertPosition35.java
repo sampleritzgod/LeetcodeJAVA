9cc1d2440b79884a30c53bdcc43c870c5a9a58e5
abhay <amaheta005@gmail.com>
2026-01-14 18:35:39 +0530

    searchInsertPosition35

---
+++ Day2/searchInsertPosition35.java
@@ -0,0 +1,25 @@
+package Leetcode.Day2;
+
+public class searchInsertPosition35 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        int target = 7;
+        int ans = searchInsertPosition(arr, target);
+        System.out.println(ans);
+    }
+    static int searchInsertPosition(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1 ;
+        while(start <= end){
+            int mid = start + (end - start) / 2 ;
+            if(target < arr[mid]){
+                end = mid - 1 ;
+            }else if(target > arr[mid]){
+                start = mid + 1 ;
+            }else{
+                return  mid;
+            }
+        }
+        return start;
+    }
+}
